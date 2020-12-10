888263dce9605095ce150b03e759f7d9725a0f5c
Starlight Romero <starlightromero@gmail.com>
2020-12-09 21:16:07 -0800

    updated to iterative fib

--- math/fibonacci.js
+++ math/fibonacci.js
@@ -1,23 +1,9 @@
-// const fib = n => {
-//   const numbers = [1, 1]
-//   for (let i = 2; i < n + 1; i++) {
-//     numbers.push(numbers[i - 1] + numbers[i - 2])
-//   }
-//   return numbers[n]
-// }
-
-const fib = (n, memo) => {
-  let result
-  if (memo[n]) {
-    return memo[n]
+const fib = n => {
+  const numbers = [1, 1]
+  for (let i = 2; i < n + 1; i++) {
+    numbers.push(numbers[i - 1] + numbers[i - 2])
   }
-  if (n === 0 || n === 1) {
-    result = 1
-  } else {
-    result = fib(n - 1, memo) + fib(n - 2, memo)
-  }
-  memo[n] = result
-  return result
+  return numbers[n]
 }
 
-console.log(fib(4, {}))
+console.log(fib(4))
