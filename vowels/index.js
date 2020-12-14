58d7e845e2b50fbbbfa9404a056a9d3d7b65158e
Starlight Romero <starlightromero@gmail.com>
2020-12-14 08:45:02 -0800

    [create] vowels func

---
+++ vowels/index.js
@@ -0,0 +1,12 @@
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+const vowels = str => str.replace(/[^aeiou]/gi, '').length
+
+module.exports = vowels
